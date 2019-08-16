56152e8e5a4f4c8fd0116a9773000a5cbb1c5b65
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-16 12:00:12 +0200

    Customer class created (no logic)

--- CompositionBankAccount/CompositionBankAccount.ClassLib/Customer.cs
+++ CompositionBankAccount/CompositionBankAccount.ClassLib/Customer.cs
@@ -6,5 +6,59 @@ namespace CompositionBankAccount.ClassLib
 {
     class Customer
     {
+        int id;
+        List<Account> accounts = new List<Account>();
+
+        public Customer(int id, List<Account> accounts)
+        {
+            throw new NotImplementedException();
+        }
+        public Customer(List<Account> accounts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Id
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public List<Account> Accounts
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public int Rating
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public decimal GetDebts()
+        {
+            throw new NotImplementedException();
+        }
+        public decimal GetAssets()
+        {
+            throw new NotImplementedException();
+        }
+        public decimal GetTotalBalance()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
